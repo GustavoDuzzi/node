1e8e746ad6c858fdeae9b98ebb95eeb3c58a26ec
Gustavo Duzzi <42771160+GustavoDuzzi@users.noreply.github.com>
2022-09-09 14:49:22 -0300

    Add files via upload

---
+++ node/global.js
@@ -0,0 +1,7 @@
+// console.log(global)
+globalThis.MinhaApp = Object.freeze({
+    saudacao(){
+        return 'Estou cagado'
+    },
+    nome: 'Sistema Legal'
+})
---
+++ node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'Fala Galera'
+exports.bemVindo = 'Bem-vindo ao node!'
+module.exports.ateLogo = 'Até um outro dia'
