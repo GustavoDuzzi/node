a1a252c85ba31e0c01ae3753a164e1d68fb61dd1
Gustavo Duzzi <42771160+GustavoDuzzi@users.noreply.github.com>
2022-09-13 10:07:39 -0300

    Update global.js

--- node/global.js
+++ node/global.js
@@ -1,7 +1,7 @@
 // console.log(global)
 globalThis.MinhaApp = Object.freeze({
     saudacao(){
-        return 'Estou cagado'
+        return 'Estou em todo lugar'
     },
     nome: 'Sistema Legal'
 })
